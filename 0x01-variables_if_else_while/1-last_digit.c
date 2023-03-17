fa84a1c468e7bde9ce7c2d8d26471a18a6a90544
Mando771 <mandomvn@gmail.com>
2023-03-17 13:43:08 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
 else if (lastd == 0)
 {
-printf("Last digit og %d is %d and is 0\n", n, lastd);
+printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
 else if (lastd < 6 && lastd != 0)
 {
