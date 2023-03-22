b1504ea4aa56befcd1832b853b1e9d6a408ac699
Mando771 <mandomvn@gmail.com>
2023-03-22 15:57:59 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: ALways 0
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
-
 void print_times_table(int n)
 {
-	unsigned long int i, J = 1, K = 2, j, j1, k, k1;
+	int num, mult, prod;
 
-	printf("%lu", J);
-	for (i = 1; i < 91; i++)
+	if (n >= 0 && n <= 15)
 	{
-		printf(", %lu", K);
-		K = K + J;
-		J = K - J;
-	}
-	j = J / 1000000000;
-	j1 = J % 1000000000;
-	k = K / 1000000000;
-	k1 = K % 1000000000;
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", k + (k1 / 1000000000));
-		printf("%lu", k1 % 1000000000);
-		k = k + j;
-		j = k - j;
-		k1 = k1 + j1;
-		j1 = k1 - j1;
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
-	printf("\n");
-	return (0);
 }
