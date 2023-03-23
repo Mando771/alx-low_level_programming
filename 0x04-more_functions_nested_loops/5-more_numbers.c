94a457ec4cfa7df26beb1f90fd1f5bb9e4935ff1
Mando771 <mandomvn@gmail.com>
2023-03-23 14:56:10 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,23 +4,23 @@
 /**
  * more_numbers - prints numbers to 14
  *
- * Return: ALways 0 
+ * Return: ALways 0
  */
 
 void more_numbers(void)
 {
-	int n1, n2;
-	
-	for (n1 = 0; n1 < 10; n1++)
-	{
-		for (n2 = 0; n2 <= 14; n2++)
-		{
-			if (n2 > 9)
-			{
-				_putchar((n2 / 10) + '0');
-			}
-			_putchar((n2 % 10) + '0');
-		}
-		_putchar(10);
-	}
+int n1, n2;
+
+for (n1 = 0; n1 < 10; n1++)
+{
+for (n2 = 0; n2 <= 14; n2++)
+{
+if (n2 > 9)
+{
+_putchar((n2 / 10) + '0');
+}
+_putchar((n2 % 10) + '0');
+}
+_putchar(10);
+}
 }
